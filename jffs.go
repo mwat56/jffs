d61c4a8439c445946438bebd7dd055e1f6584dad
Matthias Watermann <support@mwat.de>
2019-10-17 21:00:14 +0200

    + implemeted 'index.html' handling in `tOnlyFilesFilesystem.Open()`;

--- jffs.go
+++ jffs.go
@@ -5,11 +5,17 @@ package jffs
 import (
 	"net/http"
 	"os"
+	"strings"
 )
 
 /*
- * CREDITS: Brad Fitzpatrick
+ * CREDITS:
+ *
+ * Brad Fitzpatrick
  * https://groups.google.com/d/msg/golang-nuts/bStLPdIVM6w/hidTJgDZpHcJ
+ *
+ * Alex Edwards
+ * https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings
  */
 
 type (
@@ -21,17 +27,17 @@ type (
 
 	// Simple struct embedding a `http.FileSystem` that
 	// can't read directories.
-	tJustFilesFilesystem struct {
-		jffs http.FileSystem
+	tOnlyFilesFilesystem struct {
+		offs http.FileSystem
 	}
 )
 
 // Readdir reads the contents of the directory associated with file
-// and returns a slice of up to `aCount` FileInfo values, as would be
-// returned by Lstat, in directory order.
+// and returns a slice of up to `aCount` FileInfo values, as would
+// be returned by Lstat, in directory order.
 //
-// NOTE: This implementation ignores `aCount` and returns nothing, i.e.
-// both the `FileInfo` list and the `error` are `nil`.
+// NOTE: This implementation ignores `aCount` and returns nothing,
+// i.e. both the `FileInfo` list and the `error` are `nil`.
 func (f tNeuteredReaddirFile) Readdir(aCount int) ([]os.FileInfo, error) {
 	return nil, nil
 } // Readdir()
@@ -40,10 +46,19 @@ func (f tNeuteredReaddirFile) Readdir(aCount int) ([]os.FileInfo, error) {
 // that returns a `http.File` which can't read directory contents.
 //
 //	`aName` is the name of the file to open.
-func (ffs tJustFilesFilesystem) Open(aName string) (http.File, error) {
-	f, err := ffs.jffs.Open(aName)
+func (ffs tOnlyFilesFilesystem) Open(aName string) (http.File, error) {
+	f, err := ffs.offs.Open(aName)
 	if nil != err {
 		return nil, err
+	}
+
+	if fInfo, err := f.Stat(); (nil == err) && fInfo.IsDir() {
+		f.Close() // free directory's file handle
+
+		idx := strings.TrimSuffix(aName, `/`) + `/index.html`
+		if f, err = ffs.offs.Open(idx); nil != err {
+			return nil, err
+		}
 	}
 
 	return tNeuteredReaddirFile{f}, nil
@@ -55,7 +70,7 @@ func (ffs tJustFilesFilesystem) Open(aName string) (http.File, error) {
 // with the contents of the file system rooted at `aRoot`.
 //
 // To use the operating system's file system implementation,
-// use `http.Dir()``:
+// use `http.Dir()`:
 //
 //	myHandler := http.FileServer(http.Dir("/tmp")))
 //
@@ -65,7 +80,7 @@ func (ffs tJustFilesFilesystem) Open(aName string) (http.File, error) {
 //
 //	`aRoot` The root of the filesystem to serve.
 func FileServer(aRoot http.FileSystem) http.Handler {
-	return http.FileServer(tJustFilesFilesystem{aRoot})
+	return http.FileServer(tOnlyFilesFilesystem{aRoot})
 } // FileServer()
 
 /* _EoF_ */
